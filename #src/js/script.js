fa367660e62ff651336522ef5fc41b79b505c63a
Hillel1408 <etemax@bk.ru>
2023-05-08 12:55:08 +0300

    Загородные дома

--- #src/js/script.js
+++ #src/js/script.js
@@ -47,3 +47,10 @@ headerNavLinks.forEach((item) => {
         document.body.classList.remove('lock');
     });
 });
+
+const phoneInput = document.querySelectorAll('.feedback__input');
+phoneInput.forEach((item) => {
+    const phoneMask = new IMask(item, {
+        mask: '+{7}(000)000-00-00',
+    });
+});
